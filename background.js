8e447e94d5cfdc6730f285d26db44e7639457d43
Стоян Димитров <stoyan@easyads.bg>
2017-08-28 10:58:34 +0300

    Add extension icon. Show button on page reload.

--- background.js
+++ background.js
@@ -1,13 +1,32 @@
 'use strict'
 
-browser.tabs.onActivated.addListener(active => {
-  browser.pageAction.show(active.tabId)
+browser.tabs.onActivated.addListener((active) => {
+  activateAction(active.tabId)
 })
 
-browser.pageAction.onClicked.addListener(tab => {
+browser.tabs.onUpdated.addListener((tabId, changed) => {
+
+  browser.pageAction.hide(tabId)
+
+  if (changed.status
+    && changed.status !== 'complete'
+  ) {
+    return
+  }
+
+  activateAction(tabId)
+})
+
+browser.pageAction.onClicked.addListener((tab) => {
   browser.tabs.executeScript(tab.id, {
     file: 'assets/css-reload.js'
   }).catch(err => {
     // mute permission errors
   })
 })
+
+
+function activateAction(tabId)
+{
+  browser.pageAction.show(tabId)
+}
