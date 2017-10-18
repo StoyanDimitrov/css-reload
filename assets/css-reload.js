1b2bb37ee7b8f0d0fadd86dede3b1e75e2651ff2
Jean Honlet <jean.honlet@oniryx.be>
2017-10-18 10:47:24 +0200

    Add image reload on img[src] (Fix #6)

--- assets/css-reload.js
+++ assets/css-reload.js
@@ -1,10 +1,16 @@
 'use strict'
 
+let ts = Date.now();
+function addTs(href) {
+    const url = new URL(href)
+    url.searchParams.set('cacheBuster', Date.now())
+    return url.href;
+}
+
 Array
   .from(window.document.querySelectorAll('link[rel=stylesheet][href]:not([data-autoreload=false])'))
-  .map((link) => {
-    const url = new URL(link.href)
-    url.searchParams.set('cacheBuster', Date.now())
+  .map((el) => el.href = addTs(el.href));
 
-    link.href = url.href
-  })
+Array
+  .from(window.document.querySelectorAll('img[src]'))
+  .map((el) => el.src = addTs(el.src));
