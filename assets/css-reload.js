7a103e3dcc229b01eb029965bba9f9746e334dda
Стоян Димитров <stoyan@easyads.bg>
2018-06-08 14:56:43 +0300

    Craften new icon and make it visible in the Addons Mannager.

--- assets/css-reload.js
+++ assets/css-reload.js
@@ -1,10 +1,13 @@
 'use strict'
 
-Array
-  .from(window.document.querySelectorAll('link[rel=stylesheet][href]:not([data-autoreload=false])'))
-  .map((link) => {
-    const url = new URL(link.href)
-    url.searchParams.set('cacheBuster', Date.now())
+;(function() {
+  const query = 'link[rel=stylesheet][href]:not([data-autoreload=false])'
 
-    link.href = url.href
+  document.querySelectorAll(query)
+    .forEach((link) => {
+      const url = new URL(link.href)
+      url.searchParams.set('cacheBuster', Date.now())
+
+      link.href = url.href
   })
+})()
